c252e17072ffc5852c2e082f9eeac61e36936d84
Jacob D. Castro <jdcastro.business@gmail.com>
2020-05-08 23:25:28 -0700

    create working app framework

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,8 @@ import React, { Fragment } from 'react';
 import { render } from 'react-dom';
 import { AppContainer as ReactHotAppContainer } from 'react-hot-loader';
 import Root from './containers/Root';
-import { configureStore, history } from './store/configureStore';
-import './app.global.css';
+import { configureStore, history } from './redux/store/configureStore';
+import './utils/normalize.css';
 
 const store = configureStore();
 
@@ -17,3 +17,6 @@ document.addEventListener('DOMContentLoaded', () =>
     document.getElementById('root')
   )
 );
+
+// * Note!
+// ? When files/folders are renamed, saved, moved, deleted, a redux action is dispatched, which runs the side-effect of filesystem interactions via Node's fs module, gets new data from Node fs, and reducer updates filesystem state in redux store.
