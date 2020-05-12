9d44a02bbaf339551420fa646836e28a605348ed
Jacob D. Castro <jdcastro.business@gmail.com>
2020-05-11 18:25:30 -0700

    add a few reference notes

--- app/index.tsx
+++ app/index.tsx
@@ -18,5 +18,7 @@ document.addEventListener('DOMContentLoaded', () =>
   )
 );
 
-// * Note!
+// * Notes!
 // ? When files/folders are renamed, saved, moved, deleted, a redux action is dispatched, which runs the side-effect of filesystem interactions via Node's fs module, gets new data from Node fs, and reducer updates filesystem state in redux store.
+
+//  ? MDX Support: it's just react. React renders to a specified html element via id. Create a new <div id="[id]">, run a ReactDOM.render() to new div for every component embedded in the markdown file and view the thing in preview!
